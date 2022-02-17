65a8882b500dab69bdedf2c6f95547347192ca64
audgks5551 <audgks5551@daum.net>
2022-02-17 16:16:59 +0900

    member entity와repository 생성

--- src/main/java/hello/servlet/domain/Member.java
+++ src/main/java/hello/servlet/domain/Member.java
@@ -7,11 +7,14 @@ import lombok.Setter;
 
 @Getter @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Member {
 
     private Long id;
     private String username;
     private int age;
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 }
--- src/main/java/hello/servlet/domain/MemberRepository.java
+++ src/main/java/hello/servlet/domain/MemberRepository.java
@@ -3,7 +3,9 @@ package hello.servlet.domain;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -16,6 +18,24 @@ public class MemberRepository {
 
     public static MemberRepository getInstance() {
         return instance;
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 
 }
