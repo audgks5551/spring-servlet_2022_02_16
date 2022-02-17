9091d5d1f6c744544ab84e5fbb197d509a178e89
audgks5551 <audgks5551@daum.net>
2022-02-17 23:55:31 +0900

    v2 mvc

---
+++ src/main/java/hello/servlet/web/sevlet/frontcontroller/ModelView.java
@@ -0,0 +1,4 @@
+package hello.servlet.web.sevlet.frontcontroller;
+
+public class ModelView {
+}
--- src/main/java/hello/servlet/web/sevlet/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/sevlet/frontcontroller/MyView.java
@@ -5,7 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.rmi.ServerException;
 
 public class MyView {
     private String viewPath;
@@ -14,7 +13,7 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
-    public void render(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
